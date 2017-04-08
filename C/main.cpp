0ab0c2ab53214b0ed4b6df733b35ec8d99ff3b83
zStrzinar <ziga.strzinar@gmail.com>
2017-04-08 10:26:00 +0200

    Test

--- C/main.cpp
+++ C/main.cpp
@@ -1,27 +1,73 @@
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+//
+// Created by ziga on 8.4.2017.
+//
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    std::cout << "Hello, World!" << std::endl;
+using namespace cv;
+using namespace std;
 
-    if(argc != 2){
-        std::cout << "Wrong number of parameters!" << std::endl;
-        return -1;
+void printHelp() {
+    std::string msg;
+    msg = "Help\n\tUsage:\n\t\t-h\t .......... for help\n\t\tpath\t ........... specify video path\n\t\tpath output\t ... specify video path and output path";
+    std::cout << msg << std::endl;
+}
+
+int main (int argc, char ** argv){
+    std::string inputPath, outputPath;
+    std::string inputFormat, outputFormat;
+    switch (argc) {
+        case 1:	{
+            printHelp();
+            return 1;
+        }
+        case 2:	{
+            if (strcmp("-h", argv[1]) == 0) {
+                printHelp();
+                return 1;
+            }
+            else {
+                inputPath = argv[1];
+                std::size_t idx = inputPath.find_last_of('.');
+                outputPath = inputPath.substr(0, idx);
+                inputFormat = inputPath.substr(idx + 1);
+                outputPath.append("_segmented.");
+                outputPath.append(inputFormat);
+                outputFormat = inputFormat;
+                break;
+            }
+        }
+        case 3: {
+            inputPath = argv[1];
+            outputPath = argv[2];
+            std::size_t idx = inputPath.find_last_of('.');
+            inputFormat = inputPath.substr(idx + 1);
+            idx = outputPath.find_last_of('.');
+            outputFormat = outputPath.substr(idx + 1);
+            break;
+        }
+        default: {
+            printHelp();
+            return 1;
+        }
     }
 
-    std::cout << "Loading input image: " << argv[1] << std::endl;
-    cv::Mat input;
-    input = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    std::cout << "Input file path: " << inputPath << std::endl;
+    //std::cout << "Input file format: " << inputFormat << std::endl;
 
-    std::cout << "Detecting edges in input image" << std::endl;
-    cv::Mat edges;
-    cv::Canny(input,edges, 10, 100);
+    std::cout << "Output file path: " << outputPath << std::endl;
+    //std::cout << "Output file format: " << outputFormat << std::endl;
 
-    cv::namedWindow("Original",CV_WINDOW_AUTOSIZE);
-    cv::imshow("Original", input);
+    std::cout << "Opening video file" << std::endl;
 
-    cv::waitKey(0);
-    return 0;
+    cv::VideoCapture inputVideo(inputPath);
+    if (!inputVideo.isOpened())
+        std::cerr << "Failed to open video file! Aborting" << endl;
+    else
+        std::cout << "Video file successfully opened" << endl;
+
+
 }
